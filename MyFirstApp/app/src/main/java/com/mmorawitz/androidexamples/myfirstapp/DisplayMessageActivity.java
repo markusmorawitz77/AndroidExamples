83fc2f83be0ad4aa3afd7f96ab4626cacf57cc5a
Markus Morawitz <markus.morawitz@gmx.com>
2015-10-18 12:30:16 +0200

    Start another activity.

---
+++ MyFirstApp/app/src/main/java/com/mmorawitz/androidexamples/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,35 @@
+package com.mmorawitz.androidexamples.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mmorawitz.androidexamples.myfirstapp.displaymessageactivity.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+
+
+}
